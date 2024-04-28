d78e93fb4943ad89d1c14c0409bdcc5b050c6071
nayaksuvendu <nayaksuvendu78@gmail.com>
2024-04-28 08:02:16 +0530

    all commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"], // * refers to applicable for all js and html in "dist" file
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
